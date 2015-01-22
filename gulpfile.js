05a7eaffed5d8277344eeda70535a779f7a44f26
Nate <nate@codekoalas.com>
2015-01-21 22:57:30 -0600

    Adjusted some documentation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,31 @@
 /* jshint -W079 */
-var
-  gulp = require('gulp'),
-  semver = require('semver'),
-  $ = require('gulp-load-plugins')();
+var gulp   = require('gulp'),
+    semver = require('semver'),
+    $      = require('gulp-load-plugins')();
 
-var inc = function(importance) {
+var inc = function (importance) {
   return gulp.src(['./package.json'])
-  .pipe($.bump({type: importance}))
-  .pipe(gulp.dest('./'))
-  .pipe($.git.commit('Release v' + semver.inc(
+    .pipe($.bump({type: importance}))
+    .pipe(gulp.dest('./'))
+    .pipe($.git.commit('Release v' + semver.inc(
       require(__dirname + '/package.json').version,
       importance)))
-  .pipe($.tagVersion())
-  .pipe($.git.push('origin', 'master', { args: '--tags' }));
+    .pipe($.tagVersion())
+    .pipe($.git.push('origin', 'master', {args: '--tags'}));
 };
 
-
 /* JS linting ------------------------------------------------------------ */
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src('./index.js')
-  .pipe($.jshint({
-    lookup: true
-  }))
-  .pipe($.jshint.reporter('jshint-stylish'))
-  .pipe($.jshint.reporter('fail'))
-  .pipe($.jsvalidate());
+    .pipe($.jshint({
+      lookup: true
+    }))
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'))
+    .pipe($.jsvalidate());
 });
 
-
 /* Version bumping ------------------------------------------------------- */
-gulp.task('patch', ['lint'], function() { return inc('patch'); });
-gulp.task('feature', ['lint'], function() { return inc('minor'); });
-gulp.task('release', ['lint'], function() { return inc('major'); });
+gulp.task('patch', ['lint'], function () { return inc('patch'); });
+gulp.task('feature', ['lint'], function () { return inc('minor'); });
+gulp.task('release', ['lint'], function () { return inc('major'); });
