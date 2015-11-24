6549b96898ae8bb90888c4f6231a9a96fa44c25f
Nathaniel Inman <nate041010@gmail.com>
2015-11-24 07:50:42 -0600

    Updated versions of all dependencies that were outdated for dev.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,15 @@
-/// <reference path="typings/node/node.d.ts"/>
-// Load plugins
-var gulp     = require('gulp'),
-    semver   = require('semver'),
-    bump     = require('gulp-bump'),
-    git      = require('gulp-git'),
-    tag      = require('gulp-tag-version');
+import gulp from 'gulp';
+import bump from 'gulp-bump';
+import git  from 'gulp-git';
+import tag  from 'gulp-tag-version';
 
-var inc = function (importance) {
-  return gulp.src(['./package.json'])
-    .pipe(bump({type: importance}))
-    .pipe(gulp.dest('./'))
-    .pipe(git.commit('Release v' + semver.inc(
-      require(__dirname + '/package.json').version,
-      importance)))
-    .pipe(tag())
-    .on('end',function(){
-      this.pipe(git.push('origin','master', {args: '--tags'}))
-    });
-};
+var update = (importance)=> gulp
+  .src(['./package.json'])
+  .pipe(bump({type: importance}))
+  .pipe(gulp.dest('./'))
+  .pipe(git.commit('Updated version.'))
+  .pipe(tag());
 
-// Version bumping
-gulp.task('patch', function () { return inc('patch'); });
-gulp.task('feature', function () { return inc('minor'); });
-gulp.task('release', function () { return inc('major'); });
+gulp.task('patch',  ()=> update('patch'));
+gulp.task('feature',()=> update('minor'));
+gulp.task('release',()=> update('major'));
