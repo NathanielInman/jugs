4b69c3b5703e540e2ad73910aeffd27c073b5d22
Nathaniel Inman <nate@theoestudio.com>
2022-09-16 12:50:53 -0500

    automatically npm installs, fixed images

--- index.js
+++ index.js
@@ -1,88 +1,105 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import gulp from 'gulp';
 import inquirer from 'inquirer';
 import iniparser from 'iniparser';
-import conflict from 'gulp-conflict';
-import install from 'gulp-install';
 import rename from 'gulp-rename';
+import shell from 'gulp-shell';
+import tap from 'gulp-tap';
 import template from 'gulp-template';
+import filter from 'gulp-filter';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const handleDefaults = answers => answers;
 const defaults = (() => {
-  const homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
-        workingDirName = process.cwd().split('/').pop().split('\\').pop(),
-        osUserName = homeDir && homeDir.split('/').pop() || 'root',
-        configFile = homeDir + '/.gitconfig';
+  const homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+  const workingDirName = process.cwd().split('/').pop().split('\\').pop();
+  const osUserName = (homeDir && homeDir.split('/').pop()) || 'root';
+  const configFile = `${homeDir}/.gitconfig`;
+
   let user = {};
 
   if (fs.existsSync(configFile)) {
     user = iniparser.parseSync(configFile).user;
-  } //end if
+  } // end if
   return {
-    appName    : workingDirName,
-    userName   : user.name || osUserName,
-    authorEmail: user.email || '',
+    appName: workingDirName,
+    userName: user.name || osUserName,
+    authorEmail: user.email || ''
   };
 })();
 
-// If they don't specify specifics, we need to have defaults ready
-var handleDefaults = function (answers) {
-  return answers;
-};
-
-inquirer.prompt([{
-    name   : 'name',
+inquirer.prompt([
+  {
+    name: 'name',
     message: 'Give your app a name',
     default: defaults.appName
   }, {
-    name   : 'appVersion',
+    name: 'appVersion',
     message: 'What is the version of your project?',
     default: '0.1.0'
   }, {
-    name   : 'appDescription',
+    name: 'appDescription',
     message: 'What is a description of your project?',
     default: 'n/a'
   }, {
-    name   : 'authorName',
+    name: 'authorName',
     message: 'What is the author name?',
     default: defaults.userName
   }, {
-    name   : 'authorEmail',
+    name: 'authorEmail',
     message: 'What is the author email?',
     default: defaults.authorEmail
   }, {
-    name   : 'userName',
+    name: 'userName',
     message: 'What is the github username?',
     default: defaults.userName
   }, {
-    type   : 'confirm',
-    name   : 'moveon',
+    type: 'confirm',
+    name: 'moveon',
     message: 'Continue?'
-  }])
-  .then(answers=>{
-    if (!answers.moveon) return done();
+  }
+])
+  .then(answers => {
+    if (!answers.moveon) return;
+    const unsafeExtensions = ['*', '!*.png', '!*.txt', '!*.ico'];
+    const publicFolderFilter = filter(unsafeExtensions, { restore: true });
+
     answers.file = { relative: '<%= file.relative %>' };
     answers = handleDefaults(answers);
     answers.year = (new Date()).getFullYear();
-    gulp.src([
-      __dirname + '/templates/app/**'
-    ])
-      .pipe(template(answers, {
-        interpolate: /<%=\s([\s\S]+?)%>/g
-      }))
-      .pipe(rename(function (file) {
-        if (file.basename[0] === '_' && file.basename[1] === '_'){
+    const stream = gulp.src([`${__dirname}/templates/app/**`])
+      .pipe(publicFolderFilter)
+      .pipe(template(answers, { interpolate: /<%=\s([\s\S]+?)%>/g }))
+      .pipe(rename(file => {
+        if (file.basename[0] === '_' && file.basename[1] === '_') {
           file.basename = file.basename.slice(1);
         } else if (file.basename[0] === '_') {
           file.basename = '.' + file.basename.slice(1);
-        } //end if
+        } // end if
       }))
-      .pipe(conflict('./'))
-      .pipe(gulp.dest('./'))
-      .pipe(install());
+      .pipe(publicFolderFilter.restore)
+      .pipe(tap(file => {
+        console.log(
+          chalk.gray('[') +
+          chalk.black(new Date().toLocaleTimeString()) +
+          chalk.gray('] [') +
+          chalk.cyan('conflict') +
+          chalk.gray('] ') +
+          chalk.white('Creating ') +
+          chalk.magenta(file.relative || file.dirname)
+        );
+      }))
+      .pipe(gulp.dest('./'));
+
+    stream.on('end', () => {
+      console.log(chalk.green('Files created successfully.'));
+      console.log(chalk.gray('Installing npm modules...'));
+      shell.task('npm i')();
+    });
   });
